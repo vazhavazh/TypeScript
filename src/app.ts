723f46e7dbbacaa8666116861671038f21ddc8e6
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-04-09 23:47:14 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -226,57 +226,60 @@
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-class Key {
-	private signature: number;
+// class Key {
+// 	private signature: number;
 
-	constructor() {
-		this.signature = Math.random();
-	}
+// 	constructor() {
+// 		this.signature = Math.random();
+// 	}
 
-	getSignature(): number {
-		return this.signature;
-	}
-}
+// 	getSignature(): number {
+// 		return this.signature;
+// 	}
+// }
 
-class Person {
-	constructor(private key: Key) {}
-	getKey(): Key {
-		return this.key;
-	}
-}
+// class Person {
+// 	constructor(private key: Key) {}
+// 	getKey(): Key {
+// 		return this.key;
+// 	}
+// }
 
-abstract class House {
-	protected door = false;
-	private tenants: Person[] = [];
-	constructor(protected key: Key) {}
+// abstract class House {
+// 	protected door = false;
+// 	private tenants: Person[] = [];
+// 	constructor(protected key: Key) {}
 
-	comeIn(person: Person): void {
-		if (!this.door) {
-			throw new Error("Door is close");
-		}
+// 	comeIn(person: Person): void {
+// 		if (!this.door) {
+// 			throw new Error("Door is close");
+// 		}
 
-		this.tenants.push(person);
-		console.log("Person inside");
-	}
+// 		this.tenants.push(person);
+// 		console.log("Person inside");
+// 	}
 
-	abstract openDoor(key: Key): boolean;
-}
+// 	abstract openDoor(key: Key): boolean;
+// }
 
-class MyHouse extends House {
-	openDoor(key: Key) {
-		if (key.getSignature() !== this.key.getSignature()) {
-			throw new Error("Key to another door");
-		}
+// class MyHouse extends House {
+// 	openDoor(key: Key) {
+// 		if (key.getSignature() !== this.key.getSignature()) {
+// 			throw new Error("Key to another door");
+// 		}
 
-		return (this.door = true);
-	}
-}
+// 		return (this.door = true);
+// 	}
+// }
 
-const key = new Key();
+// const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+// const house = new MyHouse(key);
+// const person = new Person(key);
 
-house.openDoor(person.getKey());
+// house.openDoor(person.getKey());
 
-house.comeIn(person);
+// house.comeIn(person);
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
